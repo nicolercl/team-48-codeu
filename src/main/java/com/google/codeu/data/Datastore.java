c16675b876c96a0fdd2146cc6475b3d3975726d6
nicole <nicolercl1103@gmail.com>
2019-06-01 18:29:26 +0800

    Add user list servlet

--- src/main/java/com/google/codeu/data/Datastore.java
+++ src/main/java/com/google/codeu/data/Datastore.java
@@ -27,6 +27,8 @@ import com.google.appengine.api.datastore.Query.SortDirection;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
+import java.util.Set;
+import java.util.HashSet;
 
 /**
  * Provides access to the data stored in Datastore.
@@ -116,5 +118,15 @@ public class Datastore {
         }
 
         return messages;
+    }
+
+    public Set<String> getUsers() {
+        Set<String> users = new HashSet<>();
+        Query query = new Query("Message");
+        PreparedQuery results = datastore.prepare(query);
+        for (Entity entity : results.asIterable()) {
+            users.add((String) entity.getProperty("user"));
+        }
+        return users;
     }
 }
