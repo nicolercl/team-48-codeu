c799b27a89d5e8d350b1f9fcf431309fcfeaf22e
Sinyu104 <brandyhuang104@gmail.com>
2019-06-07 02:42:10 +0800

    allow users using BBcode

--- src/main/java/com/google/codeu/servlets/MessageServlet.java
+++ src/main/java/com/google/codeu/servlets/MessageServlet.java
@@ -23,7 +23,9 @@ import com.google.codeu.data.Message;
 import com.google.gson.Gson;
 
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -32,9 +34,15 @@ import javax.servlet.http.HttpServletResponse;
 import org.jsoup.Jsoup;
 import org.jsoup.safety.Whitelist;
 
+
 /**
  * Handles fetching and saving {@link Message} instances.
  */
+        
+
+/** Handles fetching and saving {@link Message} instances. */
+
+
 @WebServlet("/messages")
 public class MessageServlet extends HttpServlet {
 
@@ -82,7 +90,40 @@ public class MessageServlet extends HttpServlet {
         }
 
         String user = userService.getCurrentUser().getEmail();
-        String text = Jsoup.clean(request.getParameter("text"), Whitelist.none());
+        String text = request.getParameter("text");
+        //String text = Jsoup.clean(request.getParameter("text"), Whitelist.none());
+
+
+        Map<String, String> bbMap = new HashMap<String, String>();
+        bbMap.put("(\r\n|\r|\n|\n\r)", "<br/>");
+        bbMap.put("\\[b\\](.+?)\\[/b\\]", "<strong>$1</strong>");
+        bbMap.put("\\[i\\](.+?)\\[/i\\]", "<span style='font-style:italic;'>$1</span>");
+        bbMap.put("\\[u\\](.+?)\\[/u\\]", "<span style='text-decoration:underline;'>$1</span>");
+        bbMap.put("\\[h1\\](.+?)\\[/h1\\]", "<h1>$1</h1>");
+        bbMap.put("\\[h2\\](.+?)\\[/h2\\]", "<h2>$1</h2>");
+        bbMap.put("\\[h3\\](.+?)\\[/h3\\]", "<h3>$1</h3>");
+        bbMap.put("\\[h4\\](.+?)\\[/h4\\]", "<h4>$1</h4>");
+        bbMap.put("\\[h5\\](.+?)\\[/h5\\]", "<h5>$1</h5>");
+        bbMap.put("\\[h6\\](.+?)\\[/h6\\]", "<h6>$1</h6>");
+        bbMap.put("\\[quote\\](.+?)\\[/quote\\]", "<blockquote>$1</blockquote>");
+        bbMap.put("\\[p\\](.+?)\\[/p\\]", "<p>$1</p>");
+        bbMap.put("\\[p=(.+?),(.+?)\\](.+?)\\[/p\\]", "<p style='text-indent:$1px;line-height:$2%;'>$3</p>");
+        bbMap.put("\\[center\\](.+?)\\[/center\\]", "<div align='center'>$1");
+        bbMap.put("\\[align=(.+?)\\](.+?)\\[/align\\]", "<div align='$1'>$2");
+        bbMap.put("\\[color=(.+?)\\](.+?)\\[/color\\]", "<span style='color:$1;'>$2</span>");
+        bbMap.put("\\[size=(.+?)\\](.+?)\\[/size\\]", "<span style='font-size:$1;'>$2</span>");
+        bbMap.put("\\[img\\](.+?)\\[/img\\]", "<img src='$1' />");
+        bbMap.put("\\[img=(.+?),(.+?)\\](.+?)\\[/img\\]", "<img width='$1' height='$2' src='$3' />");
+        bbMap.put("\\[email\\](.+?)\\[/email\\]", "<a href='mailto:$1'>$1</a>");
+        bbMap.put("\\[email=(.+?)\\](.+?)\\[/email\\]", "<a href='mailto:$1'>$2</a>");
+        bbMap.put("\\[url\\](.+?)\\[/url\\]", "<a href='$1'>$1</a>");
+        bbMap.put("\\[url=(.+?)\\](.+?)\\[/url\\]", "<a href='$1'>$2</a>");
+        bbMap.put("\\[youtube\\](.+?)\\[/youtube\\]", "<object width='640' height='380'><param name='movie' value='http://www.youtube.com/v/$1'></param><embed src='http://www.youtube.com/v/$1' type='application/x-shockwave-flash' width='640' height='380'></embed></object>");
+        bbMap.put("\\[video\\](.+?)\\[/video\\]", "<video src='$1' />");
+
+        for (Map.Entry entry : bbMap.entrySet()) {
+            text = text.replaceAll(entry.getKey().toString(), entry.getValue().toString());
+        }
 
         Message message = new Message(user, text);
         datastore.storeMessage(message);
